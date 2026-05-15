cffe28d6b03057dc29fe9ca150cffcfadc22f786
Joshua Whitfield <ikodane842@gmail.com>
2026-05-14 21:04:18 -0700

    first commit

--- syntax/lexer/lexer.go
+++ syntax/lexer/lexer.go
@@ -1,1 +1,26 @@
 package lexer
+
+type Lexer struct {
+	input  []rune
+	output []LexerToken
+	pos    int
+}
+
+func (l *Lexer) Set(s string) {
+	l.input = []rune(s)
+}
+
+func (l *Lexer) Consume() rune {
+	ch := l.input[l.pos]
+	l.pos++
+	return ch
+}
+
+func (l *Lexer) AddToken(_name string, _type TokenType, _line int) {
+	new_token := NewToken(_name, _type, _line)
+	l.output = append(l.output, new_token)
+}
+
+func (l *Lexer) Tokenize() {
+
+}
--- syntax/lexer/token.go
+++ syntax/lexer/token.go
@@ -6,7 +6,7 @@ type LexerToken struct {
 	Line int
 }
 
-func new(_name string, _type TokenType, _line int) LexerToken {
+func NewToken(_name string, _type TokenType, _line int) LexerToken {
 	return LexerToken{
 		Name: _name,
 		Type: _type,
